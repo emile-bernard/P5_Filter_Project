56be2799a6d7478e5c1320d3e1434c01fc6c764e
Emile05 <emile.bernard.2@ulaval.ca>
2018-12-11 19:53:43 -0500

    added filters switch

--- sketch.js
+++ sketch.js
@@ -1,6 +1,8 @@
 let video;
 let poseNet;
 
+let img;
+
 let noseX = 0;
 let noseY = 0;
 
@@ -14,12 +16,36 @@ let leftEarY = 0;
 let rightEarX = 0;
 let rightEarY = 0;
 
+let selectedFilter = 'none';
+
+document.getElementById("none-choice").addEventListener("click", function(){
+    // console.log('none!');
+    switchFilter('none');
+});
+
+document.getElementById("glasses-choice").addEventListener("click", function(){
+    // console.log('glasses!');
+    switchFilter('glasses');
+});
+
+document.getElementById("clown-choice").addEventListener("click", function(){
+    // console.log('clown!');
+    switchFilter('clown');
+});
+
+document.getElementById("cat-choice").addEventListener("click", function(){
+    // console.log('cat!');
+    switchFilter('cat');
+});
+
 function setup() {
 	createCanvas(800, 650);
 	video = createCapture(video);
 	video.hide();
 	poseNet = ml5.poseNet(video, modelReady);
 	poseNet.on('pose', gotPoses);
+
+    img = loadImage("assets/cat-ear.png");  // Load the image
 }
 
 function gotPoses(poses) {
@@ -57,34 +83,55 @@ function modelReady() {
 	console.log('Model is ready');
 }
 
+function switchFilter(filter) {
+	selectedFilter = filter;
+}
+
 function draw() {
-	image(video, 0, 0);
+    image(video, 0, 0);
 
-	//nose
-	// fill(255, 0, 0);
-	// ellipse(noseX, noseY, 50);
+	if(selectedFilter == 'none') {
+		drawNone();
+	}
+    if(selectedFilter == 'glasses') {
+        drawGlasses();
+    }
+    if(selectedFilter == 'clown') {
+        drawClown();
+    }
+    if(selectedFilter == 'cat') {
+        drawCat();
+    }
+}
 
-	//eyes
-	fill(0,110, 0, 10);
-	ellipse(leftEyeX, leftEyeY, 50);
-	ellipse(rightEyeX, rightEyeY, 50);
+function drawNone() {
 
-  //left branch
-  fill(51);
-	rect(leftEyeX+20, leftEyeY, (leftEarX-leftEyeX), 10);
+}
 
-  //right branch
-  fill(51);
-	rect(rightEyeX-20, rightEyeY, (rightEarX-rightEyeX), 10);
+function drawGlasses() {
+    //eyes
+    fill(0,110, 0, 10);
+    ellipse(leftEyeX, leftEyeY, 50);
+    ellipse(rightEyeX, rightEyeY, 50);
 
-  //cross section
-  fill(51);
-	rect(leftEyeX+((rightEyeX-leftEyeX)/3), leftEyeY, (rightEyeX-leftEyeX)/3, 10);
+    //left branch
+    fill(51);
+    rect(leftEyeX+20, leftEyeY, (leftEarX-leftEyeX), 10);
 
-	//ears
-	// triangle(x1, y1, x2, y2, x3, y3);
-	// fill(200, 200, 0);
-	// triangle(leftEarX, leftEarY, leftEarX+30, leftEarY+30, leftEarX+30, leftEarY);
-	// triangle(rightEarX, rightEarY, rightEarX+30, rightEarY-30, rightEarX-30, rightEarY);
+    //right branch
+    fill(51);
+    rect(rightEyeX-20, rightEyeY, (rightEarX-rightEyeX), 10);
 
+    //cross section
+    fill(51);
+    rect(leftEyeX+((rightEyeX-leftEyeX)/3), leftEyeY, (rightEyeX-leftEyeX)/3, 10);
+}
+
+function drawClown() {
+    fill(255, 0, 0);
+    ellipse(noseX, noseY, 50);
+}
+
+function drawCat() {
+    image(img, noseX-120, noseY-200, img.width/5, img.height/5);
 }
